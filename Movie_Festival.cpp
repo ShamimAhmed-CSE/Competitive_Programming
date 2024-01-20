cc2adda65c752985d14b97d52e0f0451d2767fe1
Shamim Ahmed <shamim.cse.2024@gmail.com>
2024-01-20 14:52:33 +0600

    Add files via upload

---
+++ Movie_Festival.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(void)
+{
+    ll n;
+    cin>>n;
+
+    vector<pair<ll, ll>> t(n);
+
+    for (ll i=0; i<n; i++)
+    {
+
+        cin>>t[i].first>>t[i].second;
+    }
+    sort(t.begin(),t.end());
+
+    /*for (ll i=0; i<n; i++)
+    {
+
+        cout<<t[i].first<<" "<<t[i].second<< endl;
+    }*/
+    ll ans = 0, current = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            current = t[i].second;
+            ans++;
+        }
+        else
+        {
+            if (t[i].first >= current)
+            {
+                current = t[i].second;
+                ans++;
+            }
+            else
+            {
+                current = min(current, t[i].second);
+            }
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
